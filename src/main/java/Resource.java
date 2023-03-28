b3b6ab056464b8d81fe4aa9fd70fe0960797955c
abmkrtchyan <abmkrtchyan@ispras.ru>
2023-03-28 19:05:06 +0400

    add decrement method to Resource

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -1,4 +1,33 @@
+import java.util.concurrent.Semaphore;
+
 class Resource {
-    static int countOfItems = 0;
+    long countOfItems;
+    long capacity;
+    static Semaphore items;
+    static Semaphore spaces;
+    static Semaphore mutex;
+
+    Resource(int capacity) {
+        Resource.items = new Semaphore(0);
+        Resource.spaces = new Semaphore(capacity);
+        Resource.mutex = new Semaphore(1);
+        this.capacity = capacity;
+        this.countOfItems = 0;
+    }
+
+    void decrement(String threadName) {
+        try {
+            items.acquire();
+            mutex.acquire();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        this.countOfItems--;
+        System.out.printf("%s consumed item: %d\n", threadName, this.countOfItems);
+
+        mutex.release();
+        spaces.release();
+    }
 
 }
