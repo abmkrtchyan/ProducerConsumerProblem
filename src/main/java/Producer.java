7ae68cff0f41929898641799ba7b0f353979c538
abmkrtchyan <abmkrtchyan@ispras.ru>
2023-03-28 19:04:24 +0400

    add run method to Producer

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,19 +1,26 @@
-import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 public class Producer extends Thread {
-    Semaphore sem;
+    Resource resource;
     String threadName;
 
-    public Producer(Semaphore sem, String threadName) {
+    public Producer(Resource resource, String threadName) {
         super(threadName);
-        this.sem = sem;
+        this.resource = resource;
         this.threadName = threadName;
+        this.start();
     }
 
     @Override
     public void run() {
+        Random rand = new Random();
+        while (true) {
+            try {
+                sleep(rand.nextInt(100));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.resource.increment(this.threadName);
+        }
     }
 }
-
-
-
