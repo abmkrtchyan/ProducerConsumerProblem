33510ed526c5b54971b58e8d69c9b9762c5ecd5c
abmkrtchyan <abmkrtchyan@ispras.ru>
2023-03-28 13:54:43 +0400

    start treads in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Semaphore sem = new Semaphore(5);
+        Producer producer = new Producer(sem, "producer");
+        Consumer consumer = new Consumer(sem, "consumer");
+
+        producer.start();
+        consumer.start();
     }
 }
